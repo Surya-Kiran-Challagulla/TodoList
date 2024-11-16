27403ef3b84379be11c77437f3fa98ee8b21f028
Surya Kiran Challagulla <csk@Suryas-MacBook-Pro-2.local>
2024-11-16 13:57:25 -0600

    https://github.com/Surya-Kiran-Challagulla/TodoList.git

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 const App = () => {
-  const [task,setTask] = useState("");
-  const [todos,setTodos] = useState([]);
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState('');
 
-  const changeHandler = e =>{
-    setTask(e.target.value)
-  }
-  const submitHandler = e =>{
+  const changeHandler = (e) => {
+    setTask(e.target.value);
+    setError('');
+  };
+  const submitHandler = (e) => {
     e.preventDefault();
-    const newTodos = [...todos,task];
+    if (task.trim() === '') {
+      setError('Please enter a Value');
+      return;
+    }
+    const newTodos = [...todos, { text: task, markedForDeletion: false }];
     setTodos(newTodos);
-    setTask("");
-  }
-  const deleteHandler = (indexValue) =>{
-    const newTodos = todos.filter((todo,index) => index !== indexValue);
+    setTask('');
+  };
+  const deleteHandler = (indexValue) => {
+    const newTodos = todos.map((todo, index) =>
+      index === indexValue ? { ...todo, markedForDeletion: true } : todo
+    );
     setTodos(newTodos);
-  }
+  };
+  const updateHandler = (indexValue, newText) => {
+    const newTodos = todos.map((todo, index) =>
+      index === indexValue ? { ...todo, text: newText, markedForDeletion: false } : todo
+    );
+    setTodos(newTodos);
+  };
+  const toggleCheckbox = (indexValue) => {
+    const newTodos = todos.map((todo, index) =>
+      index === indexValue ? { ...todo, markedForDeletion: !todo.markedForDeletion } : todo
+    );
+    setTodos(newTodos);
+  };
+  const selectAllHandler = () => {
+    const newTodos = todos.map((todo) => ({ ...todo, markedForDeletion: true }));
+    setTodos(newTodos);
+  };
+  const deleteAllSelectedHandler = () => {
+    const newTodos = todos.filter((todo) => !todo.markedForDeletion);
+    setTodos(newTodos);
+  };
   return (
     <div>
       <center>
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Todo Management Application</h5>
+            <h3 className="card-title">Todo Task's</h3>
             <form onSubmit={submitHandler}>
-              <input size="30" type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
-              <input type="submit" value="Add" name="Add"/>
+              <input size="30" type="text" name="task" value={task} onChange={changeHandler} />&nbsp;&nbsp;
+              <input type="submit" value="Add" name="Add" />
+              {error && (<p style={{border: '1px solid red',borderRadius: '4px',padding: '4px 8px',color: 'red',
+                    display: 'inline-block',marginTop: '10px',fontSize: '14px',}}>{error}</p>)}
             </form>
-            <TodoList todolist={todos} deleteHandler={deleteHandler}/>
+            <button onClick={selectAllHandler}>Select All</button>&nbsp;&nbsp;
+            <button onClick={deleteAllSelectedHandler}>Delete All</button>
+            <TodoList todoList={todos} deleteHandler={deleteHandler} updateHandler={updateHandler} toggleCheckbox={toggleCheckbox} />
           </div>
         </div>
       </center>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const TodoList = ({todolist,deleteHandler}) => {
-    return (
-        <div>
-            {todolist.map((todo,index) =>
-            <div key={index}>
-                <h5>{todo} &nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h5>
-            </div>)}
-        </div>
-    )
-}
+const TodoList = ({ todoList, deleteHandler, updateHandler, toggleCheckbox }) => {
+  return (
+    <ul style={{ listStyleType: 'none', padding: 0 }}>
+      {todoList.map((todo, index) => (
+        <li key={index} style={{ marginBottom: '10px', textDecoration: todo.markedForDeletion ? 'line-through' : 'none' }}>
+          <input type="checkbox"checked={todo.markedForDeletion}onChange={() => toggleCheckbox(index)}/>&nbsp;
+          {todo.text}&nbsp;&nbsp;
+          <button onClick={() => deleteHandler(index)}>Delete</button>&nbsp;&nbsp;
+          <button onClick={() => updateHandler(index, prompt('Edit Task', todo.text))}>Edit</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default TodoList
+export default TodoList;
